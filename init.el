d76d5d2a72ac560cfa8e495653472404180ecd32
Radu Chindris <radu.chindris@gmail.com>
2023-09-19 12:05:37 +0300

    updated emacs config using org-babel

--- init.el
+++ init.el
@@ -1,28 +1,13 @@
-;; add MELPA package server
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-
-;; Uncomment this to update the package repo at each startup
-;; (unless package-archive-contents
-;;   (package-refresh-contents))
-(package-initialize)
-
-;; if not yet installed, install package use-package
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-;; load org package and our emacs-config.org file
-(require 'org)
-(org-babel-load-file "~/.emacs.d/configuration.org") 
+(org-babel-load-file "~/.emacs.d/Emacs.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(solarized-theme use-package)))
+ '(package-selected-packages
+   '(org-roam lsp-ivy lsp-ui lsp-mode csharp-mode counsel ivy nano-modeline nano-theme no-littering exec-path-from-shell use-package))
+ '(widget-image-enable nil)
+ '(x-underline-at-descent-line t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
